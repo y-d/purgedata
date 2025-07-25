b2060ee94b23d20bb1c4c56944006362da32d9c9
Red <firetouchinterest@proton.me>
2025-07-25 17:25:20 +0300

    Update lenv.lua

--- lenv.lua
+++ lenv.lua
@@ -4,7 +4,7 @@ workspace.FinishPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPar
 wait(1)
 
 game.Players.LocalPlayer.OnTeleport:Connect(function()
-	queue_on_teleport(scr)
+	queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/y-d/purgedata/refs/heads/main/lenv.lua')()")
 end)
 
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
